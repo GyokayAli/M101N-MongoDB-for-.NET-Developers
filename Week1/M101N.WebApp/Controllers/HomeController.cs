fe5068ef4a81b3b580e2597842d238bd748ba4a7
Gyokay Ali <gyokay.metin.ali@gmail.com>
2017-01-24 18:31:14 +0200

    Added week 1 tutorial sample

---
+++ Week1/M101N.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using System.Threading.Tasks;
+using M101N.WebApp.Models;
+using MongoDB.Driver;
+
+namespace M101N.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //GET: Home
+        public async Task<ActionResult> Index()
+        {
+            var client = new MongoClient();
+            var db = client.GetDatabase("test");
+            var col = db.GetCollection<Person>("people");
+
+            var person = await col.Find(x => x.Name.StartsWith("Jane")).FirstOrDefaultAsync();
+
+            return View(person);
+        }
+    }
+}
